94fe5a8f682cd6b8f734a36893518023270d039b
Ryan <durosyan@outlook.com>
2024-06-30 12:06:42 +0100

    added linux foundation

--- docs/index.md
+++ docs/index.md
@@ -5,3 +5,4 @@ This is my home page, I try to keep track of what I'm doing here
 - [linkedin](https://www.linkedin.com/in/durosyan)
 - [Microsoft](https://learn.microsoft.com/en-gb/users/durosyan/)
 - [Systemctl.uk](https://systemctl.uk)
+- [Linux Foundation](https://openprofile.dev/profile/durosyan)
--- tooling/Debian/KernelVirtualMachine/ubuntu-server.sh
+++ tooling/Debian/KernelVirtualMachine/ubuntu-server.sh
@@ -1,14 +1,10 @@
-# install packages for libvirt/kvm
-# sudo apt -y install bridge-utils cpu-checker libvirt-clients libvirt-daemon qemu qemu-kvm
-
-# create a basic virtual machine..
-#sudo virt-install \
-#  --name ubuntu-guest \
-#  --os-variant ubuntu20.04 \
-#  --vcpus 2 \
-#  --ram 2048 \
-#  --location http://ftp.ubuntu.com/ubuntu/dists/focal/main/installer-amd64/ \
-#  --network bridge=virbr0,model=virtio \
-#  --graphics none \
-#  --extra-args='console=ttyS0,115200n8 serial'
+sudo virt-install \
+	--name ubuntu-guest \
+	--os-variant ubuntu20.04 \
+	--vcpus 2 \
+	--ram 2048 \
+	--location http://ftp.ubuntu.com/ubuntu/dists/focal/main/installer-amd64/ \
+	--network bridge=virbr0,model=virtio \
+	--graphics none \
+	--extra-args='console=ttyS0,115200n8 serial'
 
