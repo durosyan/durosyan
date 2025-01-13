80f92a68964e2a121fbde0ec96d5f82f40017f68
FeasableDenial <advised_juries.0r@icloud.com>
2025-01-13 14:44:11 +0000

    added nextjs

---
+++ next-js/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  basePath: "/FeasableDenial",
+  assetPrefix: "/FeasableDenial/",
+  output: 'export'
+};
+
+export default nextConfig;
