719a83e1aa05da278b0bff7125f329f6256fdbde
FeasableDenial <advised_juries.0r@icloud.com>
2025-01-14 10:21:52 +0000

    finished up tutorial

--- nextjs-blog/next.config.js
+++ nextjs-blog/next.config.js
@@ -1,10 +1,10 @@
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  /* config options here */
-  basePath: isProd ? "/plausible-denial" : "",
-  assetPrefix: isProd ? "/plausible-denial/" : "",
-  output: 'export'
+    images: { unoptimized: true },
+    basePath: isProd ? "/plausible-denial" : "",
+    assetPrefix: isProd ? "/plausible-denial/" : "",
+    output: 'export'
 };
 
-export default nextConfig;
+module.exports = nextConfig;
---
+++ nextjs-blog/pages/posts/[id].js
@@ -0,0 +1,44 @@
+import Layout from '../../components/layout';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+
+export default function Post({ postData }) {
+    return (
+      <Layout>
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+        <article>
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+      </Layout>
+    );
+  }
+
+export async function getStaticProps({ params }) {
+    // Add the "await" keyword like this:
+    const postData = await getPostData(params.id);
+
+    return {
+        props: {
+            postData,
+        },
+    };
+}
+
+// In development (npm run dev or yarn dev), getStaticPaths runs on every request.
+// In production, getStaticPaths runs at build time.
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
