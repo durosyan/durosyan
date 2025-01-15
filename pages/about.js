ffed62ff2c6a7ca58dc816fcda12268abd2a7928
FeasableDenial <advised_juries.0r@icloud.com>
2025-01-15 00:12:03 +0000

    text

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,23 @@
 import Head from 'next/head';
-import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
 export default function About() {
   return (
-    <Layout>
+    <Layout about>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm a passionate IT professional with experience in software engineering and cybersecurity, as well as diverse roles like 999 emergency call handler and bartender. I'm eager to advance my IT career, actively learning with Hack The Box, and aiming for my CompTIA Security+ certification. In my spare time, I enjoy bouldering, hiking, and volunteering with the British Mountaineering Council, and hold certifications in CompTIA A+, AWS Developer Associate, and Microsoft Azure Fundamentals.</p>
+        <p>Hello, I'm a passionate IT professional with experience in software engineering and cybersecurity.</p>
+        <p>My goal is to become a cybersecurity professional, and I'm currently studying for my CompTIA Security+ certification.</p>
+        <p>In my spare time, I enjoy bouldering, hiking, and volunteering with the British Mountaineering Council.</p>
+        <p>The purpose of this blog is to:</p>
+        <ol>
+          <li>Document my learning journey</li>
+          <li>Share my experiences</li>
+          <li>Help others</li>
+        </ol>
       </section>
     </Layout>
   );
