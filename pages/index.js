5312729a2a221eb0bce5b1581b9ac011bef13930
FeasableDenial <advised_juries.0r@icloud.com>
2025-01-16 10:57:49 +0000

    added search function

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
@@ -5,15 +6,34 @@ import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 import Date from '../components/date';
 
+function searchPosts(query, allPostsData) {
+  const lowerCaseQuery = query.toLowerCase();
+
+  return allPostsData.filter(post => {
+    const title = post.title.toLowerCase();
+    const tags = post.tags.map(tag => tag.toLowerCase());
+    return title.includes(lowerCaseQuery) || tags.some(tag => tag.includes(lowerCaseQuery));
+  });
+}
+
 export default function Home({ allPostsData }) {
+  const [filteredPosts, setFilteredPosts] = React.useState(allPostsData);
+
   return (
     <Layout currentPage={'home'}>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      <div className={utilStyles.search}>
+        <input
+          type="text"
+          placeholder="Search posts"
+          onChange={(e) => setFilteredPosts(searchPosts(e.target.value, allPostsData))}
+        />
+      </div>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title, tags }) => (
+          {filteredPosts.map(({ id, date, title, tags }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>{title}</Link>
               <br />
